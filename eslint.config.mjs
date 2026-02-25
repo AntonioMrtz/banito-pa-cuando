06a0a454cd4ec492b740825d0aa81db7c3e2de4a
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2026-02-25 16:11:30 +0100

    test: add cypress config and autocomplete location test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,37 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+// plugins imported to reference their exported configs
+import vitestPlugin from "eslint-plugin-vitest";
+import cypressPlugin from "eslint-plugin-cypress";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // add custom rules for hooks
   {
+    plugins: {
+      vitest: vitestPlugin,
+      cypress: cypressPlugin,
+    },
+  },
+  {
+    files: ["/__tests__/**/*.{ts,tsx,js,jsx}"],
+    extends: [vitestPlugin.configs.recommended],
     rules: {
       "react-hooks/exhaustive-deps": "error",
       "react-hooks/rules-of-hooks": "error",
     },
   },
+  {
+    files: [
+      "cypress/**/*.ts",
+      "cypress/**/*.tsx",
+      "cypress/**/*.js",
+      "cypress/**/*.jsx",
+    ],
+    extends: [cypressPlugin.configs.recommended],
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
