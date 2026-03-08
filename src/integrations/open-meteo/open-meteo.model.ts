eea3b0cace9a319266ce6f91b4608bd502ebbbd3
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2026-03-08 16:24:20 +0100

    ci: add build pipeline

--- src/integrations/open-meteo/open-meteo.model.ts
+++ src/integrations/open-meteo/open-meteo.model.ts
@@ -7,45 +7,15 @@ export {
 
 import { z } from "zod";
 
-interface OpenMeteoForecastDTO {
-  latitude: number;
-  longitude: number;
-  generationtime_ms: number;
-  utc_offset_seconds: number;
-  timezone: string;
-  timezone_abbreviation: string;
-  elevation: number;
-  hourly_units: OpenMeteoHourlyUnitsDTO;
-  hourly: OpenMeteoHourlyDataDTO;
-}
+const OpenMeteoHourlyUnitsSchema = z.object({
+  time: z.string(),
+  apparent_temperature: z.string(),
+  precipitation: z.string(),
+  windspeed_10m: z.string(),
+  wind_gusts_10m: z.string(),
+});
 
-interface OpenMeteoHourlyUnitsDTO {
-  time: string;
-  apparent_temperature: string;
-  precipitation: string;
-  windspeed_10m: string;
-  wind_gusts_10m: string;
-}
-
-interface OpenMeteoHourlyDataDTO {
-  time: string[];
-  apparent_temperature: number[];
-  precipitation: number[];
-  windspeed_10m: number[];
-  wind_gusts_10m: number[];
-}
-
-const OpenMeteoHourlyUnitsSchema: z.ZodType<OpenMeteoHourlyUnitsDTO> = z.object(
-  {
-    time: z.string(),
-    apparent_temperature: z.string(),
-    precipitation: z.string(),
-    windspeed_10m: z.string(),
-    wind_gusts_10m: z.string(),
-  },
-);
-
-const OpenMeteoHourlyDataSchema: z.ZodType<OpenMeteoHourlyDataDTO> = z.object({
+const OpenMeteoHourlyDataSchema = z.object({
   time: z.array(z.string()),
   apparent_temperature: z.array(z.number()),
   precipitation: z.array(z.number()),
@@ -53,7 +23,7 @@ const OpenMeteoHourlyDataSchema: z.ZodType<OpenMeteoHourlyDataDTO> = z.object({
   wind_gusts_10m: z.array(z.number()),
 });
 
-const OpenMeteoForecastSchema: z.ZodType<OpenMeteoForecastDTO> = z.object({
+const OpenMeteoForecastSchema = z.object({
   latitude: z.number(),
   longitude: z.number(),
   generationtime_ms: z.number(),
@@ -64,3 +34,7 @@ const OpenMeteoForecastSchema: z.ZodType<OpenMeteoForecastDTO> = z.object({
   hourly_units: OpenMeteoHourlyUnitsSchema,
   hourly: OpenMeteoHourlyDataSchema,
 });
+
+type OpenMeteoHourlyUnitsDTO = z.infer<typeof OpenMeteoHourlyUnitsSchema>;
+type OpenMeteoHourlyDataDTO = z.infer<typeof OpenMeteoHourlyDataSchema>;
+type OpenMeteoForecastDTO = z.infer<typeof OpenMeteoForecastSchema>;
--- src/integrations/photon/photon.model.ts
+++ src/integrations/photon/photon.model.ts
@@ -5,58 +5,32 @@ export {
   type PhotonFeaturePropertiesDTO,
   PhotonFeatureSchema,
 };
+const PhotonFeaturePropertiesSchema = z.object({
+  type: z.literal("Feature"),
+  properties: z.object({
+    osm_type: z.string(),
+    osm_id: z.number(),
+    osm_key: z.string(),
+    osm_value: z.string(),
+    type: z.string(),
+    postcode: z.string().optional(),
+    countrycode: z.string(),
+    name: z.string(),
+    district: z.string().optional(),
+    city: z.string().optional(),
+    county: z.string().optional(),
+    state: z.string().optional(),
+    country: z.string(),
+  }),
+  geometry: z.object({
+    type: z.literal("Point"),
+    coordinates: z.tuple([z.number(), z.number()]),
+  }),
+});
 
-interface PhotonFeatureDTO {
-  features: PhotonFeaturePropertiesDTO[];
-}
-
-interface PhotonFeaturePropertiesDTO {
-  type: "Feature";
-  properties: {
-    osm_type: string;
-    osm_id: number;
-    osm_key: string;
-    osm_value: string;
-    type: string;
-    postcode?: string;
-    countrycode: string;
-    name: string;
-    district?: string;
-    city?: string;
-    county?: string;
-    state?: string;
-    country: string;
-  };
-  geometry: {
-    type: "Point";
-    coordinates: [number, number];
-  };
-}
-
-const PhotonFeaturePropertiesSchema: z.ZodType<PhotonFeaturePropertiesDTO> =
-  z.object({
-    type: z.literal("Feature"),
-    properties: z.object({
-      osm_type: z.string(),
-      osm_id: z.number(),
-      osm_key: z.string(),
-      osm_value: z.string(),
-      type: z.string(),
-      postcode: z.string().optional(),
-      countrycode: z.string(),
-      name: z.string(),
-      district: z.string().optional(),
-      city: z.string().optional(),
-      county: z.string().optional(),
-      state: z.string().optional(),
-      country: z.string(),
-    }),
-    geometry: z.object({
-      type: z.literal("Point"),
-      coordinates: z.tuple([z.number(), z.number()]),
-    }),
-  });
-
-const PhotonFeatureSchema: z.ZodType<PhotonFeatureDTO> = z.object({
+const PhotonFeatureSchema = z.object({
   features: z.array(PhotonFeaturePropertiesSchema),
 });
+
+type PhotonFeaturePropertiesDTO = z.infer<typeof PhotonFeaturePropertiesSchema>;
+type PhotonFeatureDTO = z.infer<typeof PhotonFeatureSchema>;
